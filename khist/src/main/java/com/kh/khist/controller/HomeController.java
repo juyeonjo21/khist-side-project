1d5c605e96679f25d18e64cc1d80268911181c46
jaenyyyy <wodms0419@naver.com>
2023-12-21 20:57:27 +0900

    ㅎㅎ;

--- khist/src/main/java/com/kh/khist/controller/HomeController.java
+++ khist/src/main/java/com/kh/khist/controller/HomeController.java
@@ -1,10 +1,10 @@
-package com.kh.khis.controller;
+package com.kh.khist.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HomeContorller {
+public class HomeController {
 
 	@RequestMapping("/")
 	public String home() {

6b8bdf8cf8553d44189668256ceca174ac4f7672
Jelo777 <junheef1@gmail.com>
2024-04-24 16:17:23 +0900

    잔여파일

--- khist/src/main/java/com/kh/khist/dao/MemberDaoImpl.java
+++ khist/src/main/java/com/kh/khist/dao/MemberDaoImpl.java
@@ -1,11 +1,17 @@
 package com.kh.khist.dao;
 
+
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kh.khist.dto.MemberDto;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
 @Repository
 public class MemberDaoImpl implements MemberDao{
 

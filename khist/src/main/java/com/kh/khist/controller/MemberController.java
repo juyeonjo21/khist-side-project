f443b3c4c683467125b5a9aac211cae6d1568fe2
Jelo777 <junheef1@gmail.com>
2024-04-26 16:19:38 +0900

    비밀번호 암호화, restapi 설정

--- khist/src/main/java/com/kh/khist/controller/MemberController.java
+++ khist/src/main/java/com/kh/khist/controller/MemberController.java
@@ -1,12 +1,14 @@
 package com.kh.khist.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.khist.configuration.EncryptConfiguration;
 import com.kh.khist.dao.MemberDao;
 import com.kh.khist.dto.MemberDto;
 
@@ -18,7 +20,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/member")
 public class MemberController {
 
-	@Autowired private MemberDao memberDao;
+	@Autowired 
+	private MemberDao memberDao;
 	
 	@GetMapping("/join")
 	public String join() {
@@ -43,21 +46,23 @@ public class MemberController {
 	
 	@PostMapping("/login")
 		public String login(@ModelAttribute MemberDto memberDto, HttpSession session) {
-		String memberEmail = memberDto.getMemberEmail();
-		MemberDto findMemberDto = memberDao.selectOne(memberEmail);
-		if(findMemberDto == null) return "redirect:login?error";
-		
-		boolean correctPw = memberDto.getMemberPw().equals(findMemberDto.getMemberPw());
-		
-		if(correctPw) {
-			session.setAttribute("email", findMemberDto.getMemberEmail());
-			session.setAttribute("level", findMemberDto.getMemberLevel());
-			session.setAttribute("course", findMemberDto.getCourseNo());
+		MemberDto findDto = memberDao.login(memberDto);
+		if(findDto != null) {
+			session.setAttribute("email", findDto.getMemberEmail());
+			session.setAttribute("level", findDto.getMemberLevel());
+			session.setAttribute("course", findDto.getCourseNo());
+			return "redirect:/";
 		}
 		else {
 			return "redirect:login?error";
 		}
-		
+	}
+	
+	@RequestMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("email");
+		session.removeAttribute("level");
+		session.removeAttribute("course");
 		return "redirect:/";
 	}
 }
